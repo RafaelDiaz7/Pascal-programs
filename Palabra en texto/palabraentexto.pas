06a886e711e9afa4978bafaa1ac731993b5a737f
RafaelDiaz7 <rafaelaarondiaz123@gmail.com>
2019-01-15 17:54:25 -0400

    Cambiando nombre de las variables

--- Palabra en texto/palabraentexto.pas
+++ Palabra en texto/palabraentexto.pas
@@ -2,36 +2,44 @@ program palabraentexto;
 
 uses crt;
 
-var texto,palabra,acumulada:string;
-    i,j,contador:integer;
+var cadena_uno,cadena_dos,acumulada:string;
+    i,contador:integer;
 
 BEGIN
 
-writeln('Hola! Este programa te dice cuantas veces aparece una palabra en un texto');
-writeln('Para ello debes ingresar el texto y luego la palabra.');
+writeln('Hola! Este programa te dice cuantas veces aparece una cadena dentro de otra cadena');
+writeln('Para ello debes ingresar la primera cadena y luego la segunda cadena.');
+writeln('La segunda es la que se determinara cuantas veces aparace en la primera.');
 writeln;
-writeln('Introduzca el texto');
-readln(texto);
-writeln('Introduzca la palabra');
-readln(palabra);
+writeln('Presione cualquier tecla para continuar');
+readkey;
+clrscr;
+writeln('Introduzca la primera cadena');
+readln(cadena_uno);
+writeln('Introduzca la segunda cadena');
+readln(cadena_dos);
 
 contador:=0;
 
-for i:=1 to length(texto) do
+if cadena_uno=cadena_dos then
+    writeln('La primera cadena que ingreso');
+    writeln('es igual a la segunda.');
+
+for i:=1 to length(cadena_uno) do
       begin
-             if texto[i]=' ' then
+             if cadena_uno[i]=' ' then
                 begin
                 acumulada:=''
                 end
              else
                  begin
-                 acumulada:=acumulada+texto[i];
+                 acumulada:=acumulada+cadena_uno[i];
                  writeln(acumulada);
                  end;
 
-             if length(palabra)=length(acumulada) then
+             if length(cadena_dos)=length(acumulada) then
                 begin
-                     if palabra=acumulada then
+                     if cadena_dos=acumulada then
                         contador:=contador+1;
                 end;
       end;
